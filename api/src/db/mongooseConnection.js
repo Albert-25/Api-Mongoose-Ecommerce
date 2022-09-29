fe2331dbe6da4254aaefe40f52cdf45dc4a8fbd2
Albert-25 <ajst_95@outlook.com>
2022-09-28 22:07:40 -0500

    added .env successfully

--- api/src/db/mongooseConnection.js
+++ api/src/db/mongooseConnection.js
@@ -9,7 +9,6 @@ const {
 const CONNECTION_URL = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_DATABASE}?retryWrites=true&w=majority`;
 
 const mongooseConnection = async () => {
-    console.log("QUE PASAAAA ", CONNECTION_URL)
     await mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 }
 
